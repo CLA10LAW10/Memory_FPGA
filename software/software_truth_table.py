08afed1947a429f591e4ff6efa690a9d7205da2e
Clayton Lawton <cla10law10@gmail.com>
2023-02-21 20:46:13 -0800

    Part 5 Progress

--- software/software_truth_table.py
+++ software/software_truth_table.py
@@ -3,16 +3,18 @@
 
 file1 = open("TemperatueConversion.txt","w") # Open file
 
-for c in range(101):
+for c in range(101): # 0 - 100
+  c = bin(c)[2:]
   c = str(c)
   file1.write(c + "\n") # Write to file with line returns
-  #print(c)            # Print to terminal to verify values.
+  print(c)            # Print to terminal to verify values.
 
-for f in range(101):
+for f in range(101): # 32 - 212
   f = 9/5 * f + 32
-  f = round(f,1)
+  f = int(f)
+  f = bin(f)[2:]
   f = str(f)
   file1.write(f + "\n") # Write to file with line returns
-  #print(f)            # Print to terminal to verify values.
+  print(f)            # Print to terminal to verify values.
 
 file1.close()           # Close file
